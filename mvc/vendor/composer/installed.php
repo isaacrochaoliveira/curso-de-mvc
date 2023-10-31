31c5e0558c3e08361f1b7624fa62a78c1677fcaf
isaacrochaoliveira <120681524+isaacrochaoliveira@users.noreply.github.com>
2023-10-30 21:08:26 -0300

    Começo do Estudo de MVC com PHP

---
+++ mvc/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'isaac/mvc',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '920a1f6a5f07b138cd34a8713834520fad98e49e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'isaac/mvc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '920a1f6a5f07b138cd34a8713834520fad98e49e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
