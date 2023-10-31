98e341a07e8e2c084e3382bf9ee15cb5d4c33510
isaacrochaoliveira <120681524+isaacrochaoliveira@users.noreply.github.com>
2023-10-30 21:17:21 -0300

    Continuando

--- mvc/app/Controller/Pages/home.php
+++ mvc/app/Controller/Pages/home.php
@@ -3,5 +3,11 @@
 namespace App\Controller\Pages;
 
 class Home {
-    
+    /**
+     * Responsável por retornar o conteúdo da nossa home (view)
+     * @return string
+     */
+    public static function getHome() {
+        return "Olá, Mundo!";
+    }
 }
