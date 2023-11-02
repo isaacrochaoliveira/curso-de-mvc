4735432e83c0e0c0c286e1ae413dac5fb909f4c8
isaacrochaoliveira <120681524+isaacrochaoliveira@users.noreply.github.com>
2023-11-01 23:10:51 -0300

    MVC

--- mvc/app/Http/Router.php
+++ mvc/app/Http/Router.php
@@ -2,6 +2,9 @@
 
 namespace App\Http;
 
+use \Closure;
+use Exception;
+
 class Router {
     /**
      * Url Completa do Projeto (raiz)
@@ -14,4 +17,105 @@ class Router {
      * @var string
      */
     private $prefix = '';
+
+    /**
+     * Índice de rotas
+     * @var array
+     */
+    private $routes = [];
+
+    /**
+     * Intância de Request
+     * @var Request
+     */
+    private $request;
+
+    /**
+     * Método Responsável por iniciar a classe
+     * @param string
+     */
+    public function __construct($url) {
+        $this->request = new Request();
+
+        $this->url = $url;
+        $this->setPrefix();
+    }
+
+    /**
+     * Método Responsável por definir os prefixo das rotas
+     */
+    private function setPrefix() {
+        //Informações da URL atual
+        $parseUrl = parse_url($this->url);
+
+        //Define o Prefixo
+        $this->prefix = $parseUrl['path'] ?? '';
+    } 
+
+    /**
+     * Método Responsável por adicionar uma rota na classe
+     * @param string
+     * @param string
+     * @param array
+     */
+    private function addRoute($method, $route, $params = []) {
+        //Validação dos parâmetros
+        foreach ($params as $key => $value) {
+            if ($value instanceof Closure) {
+                $param['controller'] = $value;
+                unset($params[$key]);
+                continue;
+            }
+        }
+
+        //Padrão de Validação da URL
+        $patternRoute = '/^'. str_replace('/', '\/', $route).'$/';
+        
+        //ADD a rota dentro da classe
+
+        $this->routes[$patternRoute][$method] = $params;
+    }
+
+    /**
+     * Método Responsável por Retornar a Uri desconsiderando o prefixo
+     * @return string
+     */
+    private function getUri() {
+        // URI da Request
+        $uri = $this->request->getUri();
+
+        var_dump($uri);
+    }
+
+    /**
+     * Método Responsável por retornar os dados da rota atual
+     * @return array
+     */
+    private function getRoute() {
+        // URI
+        $uri = $this->getUri();
+    }
+
+    /**
+     * Método Responsável por executar a rota atual
+     * @return Response
+     */
+    public function run() {
+        try {
+            // Obtem a Rota atual
+            $route = $this->getRoute();
+
+        } catch (Exception $e) {
+            return new Response($e->getCode(),$e->getMessage());
+        }
+    }
+
+    /**
+     * Método Responsável por definir uma rota de GET
+     * @param string
+     * @param array
+     */
+    public function get($route, $params = []) {
+        return $this->addRoute('GET', $route, $params);
+    }
 }
--- mvc/index.php
+++ mvc/index.php
@@ -1,9 +1,23 @@
 <?php 
 require(__DIR__ . "/vendor/autoload.php");
 
+
 use \App\Controller\Pages\Home;
+use \App\Http\Router;
+use \App\Http\Response;
 
+define('URL', 'http://localhost/learn/curso-de-mvc/mvc');
 
-echo Home::getHome();
+$obRouter = new Router(URL);
+
+//Rota Home
+$obRouter->get('/', [
+    function() {
+        return new Response(200, Home::getHome());
+    }
+]);
+
+// Imprimi o Response da Rota
+$obRouter->run()->sendResponse();
 
 ?>
